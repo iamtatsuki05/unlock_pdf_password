189ce1b407ddefc3801ee1b5285e5064178d924d
Tatsuki Okada <tatsukio0522@gmail.com>
2023-06-08 13:44:17 +0900

    refactoring func

--- src/unlock_pdf_password/unlock_pdf_password.py
+++ src/unlock_pdf_password/unlock_pdf_password.py
@@ -1,25 +1,23 @@
 import itertools
 import string
+from pathlib import Path
+from typing import Optional, Union
 
 import pikepdf
 
-
 def unlock_pdf_password(
-    input_pdf_path,
-    output_pdf_path,
-    password_in_number=True,
-    password_in_lower=True,
-    password_in_upper=True,
-    password_in_sign=True,
-    password_length=None,
-    password_start_length=None,
-):
+    input_pdf_path: Union[str, Path],
+    output_pdf_path: Union[str, Path],
+    password_in_number: bool = True,
+    password_in_lower: bool = True,
+    password_in_upper: bool = True,
+    password_in_sign: bool = True,
+    password_length: Optional[int] = None,
+    password_start_length: Optional[int] = None,
+) -> int:
     chars = []
-    is_flag = False
-    if password_start_length is not None:
-        counter = password_start_length - 1
-    else:
-        counter = 0
+    counter = password_start_length - 1 if password_start_length else 0
+
     if password_in_number:
         chars.extend(list(string.digits))
     if password_in_lower:
@@ -28,20 +26,21 @@ def unlock_pdf_password(
         chars.extend(list(string.ascii_uppercase))
     if password_in_sign:
         chars.extend(list(string.punctuation))
-    while is_flag != True:
-        if password_length is not None:
-            if counter >= password_length:
-                print("Password not found")
-                return 0
+
+    while True:
+        if password_length is not None and counter >= password_length:
+            print("Password not found")
+            return 0
+
         counter += 1
         for password in itertools.product(chars, repeat=counter):
             try:
                 password = "".join(password)
-                pdf = pikepdf.open(input_pdf_path, password=password)
-                pdf_unlocked = pikepdf.new()
-                pdf_unlocked.pages.extend(pdf.pages)
-                pdf_unlocked.save(output_pdf_path)
+                with pikepdf.open(input_pdf_path, password=password) as pdf:
+                    pdf_unlocked = pikepdf.new()
+                    pdf_unlocked.pages.extend(pdf.pages)
+                    pdf_unlocked.save(output_pdf_path)
                 print(f"Password is {password}.")
                 return 0
-            except:
+            except pikepdf.PasswordError:
                 print(f"{password} is not correct.")
